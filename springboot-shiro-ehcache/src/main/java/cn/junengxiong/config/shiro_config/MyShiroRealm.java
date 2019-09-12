eaf2296876396b445e9ea907c3d089d193967ddb
NewYao <jianghui_w@163.com>
2019-09-12 16:29:08 +0800

    完成ehcache测试

--- springboot-shiro-ehcache/src/main/java/cn/junengxiong/config/shiro_config/MyShiroRealm.java
+++ springboot-shiro-ehcache/src/main/java/cn/junengxiong/config/shiro_config/MyShiroRealm.java
@@ -2,10 +2,10 @@ package cn.junengxiong.config.shiro_config;
 
 import java.util.Set;
 
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
-import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
@@ -89,20 +89,20 @@ public class MyShiroRealm extends AuthorizingRealm {
      * 重写方法,清除当前用户的的 授权缓存
      * @param principals
      */
-    @Override
-    public void clearCachedAuthorizationInfo(PrincipalCollection principals) {
-        super.clearCachedAuthorizationInfo(principals);
+    public void clearCachedAuthorizationInfo() {
+        super.clearCachedAuthorizationInfo(SecurityUtils.getSubject().getPrincipals());
     }
 
     /**
      * 重写方法，清除当前用户的 认证缓存
      * @param principals
      */
-    @Override
-    public void clearCachedAuthenticationInfo(PrincipalCollection principals) {
-        super.clearCachedAuthenticationInfo(principals);
+    public void clearCachedAuthenticationInfo() {
+        super.clearCachedAuthenticationInfo(SecurityUtils.getSubject().getPrincipals());
     }
-
+    /**
+     * 清除某个用户认证和授权缓存
+     */
     @Override
     public void clearCache(PrincipalCollection principals) {
         super.clearCache(principals);
